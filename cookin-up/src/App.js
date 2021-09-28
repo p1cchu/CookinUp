d7679dc8a10a5469b209259eb6dae2e73c8906f5
p1cchu <emjotp@gmail.com>
2021-09-28 20:40:25 +0200

    added test api call

--- cookin-up/src/App.js
+++ cookin-up/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const API_KEY = "ce6473422bf44e3f8b11c98c2e751ce6";
+  const apiURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=apples,+flour,+sugar&number=2`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`${apiURL}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's Cook This Shit App!!!</h1>
     </div>
   );
 }

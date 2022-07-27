27e9c42af11ce24989933fec04eefb5e7d89d417
M. P <emjotp@gmail.com>
2022-07-27 14:22:44 +0200

    security update

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import About from "./About";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={App} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   );
 }
